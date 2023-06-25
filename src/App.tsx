6da6ed2f4dc647ba708bc6f3251dd090d0498c68
Edilson Almeida <edifilho1409@outook.com>
2023-06-25 16:56:50 -0300

    new: banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Box } from "@chakra-ui/react";
 import { NavBar } from "./components";
+import Hero from "./components/Hero/hero";
 
 function App() {
   return (
     <Box>
       <NavBar />
+      <Hero />
     </Box>
   );
 }
---
+++ src/components/Hero/hero.tsx
@@ -0,0 +1,39 @@
+import { Box, Image, Text } from "@chakra-ui/react";
+
+const Hero = () => {
+  return (
+    <Box maxW={1640} mx="auto" p={4}>
+      <Box maxH={500} pos="relative">
+        {/* Overlay */}
+        <Box
+          pos="absolute"
+          w="full"
+          h="full"
+          textColor="gray.400"
+          maxH={500}
+          bgColor="blackAlpha.100"
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+        >
+          <Text px={4} fontSize={{ base: "4xl", sm: "5xl", md: "6xl" }}>
+            The <span style={{ color: "#DD6B20" }}>Best</span>
+          </Text>
+          <Text px={4} fontSize={{ base: "4xl", sm: "5xl", md: "6xl" }}>
+            {" "}
+            <span style={{ color: "#DD6B20" }}>Foods</span> Devlivered
+          </Text>
+        </Box>
+        <Image
+          w="full"
+          maxH={500}
+          objectFit="cover"
+          src="https://images.pexels.com/photos/1639562/pexels-photo-1639562.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt="/"
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default Hero;
